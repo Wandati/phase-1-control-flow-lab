ec62df7e7b2d18d6ea04105ab61fafc7e5cbbcb9
Wandati <Petersimon801@gmail.com>
2023-06-12 18:34:04 +0300

    Completed Assiggnment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ft){
   // Write your code here!
+  if (ft <= 400 ){
+    return "This one is on me!"
+  }else if (ft > 2000){
+    return "I will gladly take your thirty bucks."
+  }else if (ft > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === "NYC"?"Ok, sounds good.":"No go"
+  
   // Write your code here!
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+      return"Thank you so much."
+    case "not as generous":
+      return"Thank you."
+    default:
+      return"Bye."
+  }
+
+
+
   // Write your code here!
+
 }
